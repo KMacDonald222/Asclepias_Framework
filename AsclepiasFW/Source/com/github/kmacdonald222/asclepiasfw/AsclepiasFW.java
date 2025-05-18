0507c983b30e72bb8a50d5ed4f751710bcad7f31
KMacDonald222 <keeganm742@gmail.com>
2025-05-18 13:31:08 -0300

    Added Java projects for Framework and test program

---
+++ AsclepiasFW/Source/com/github/kmacdonald222/asclepiasfw/AsclepiasFW.java
@@ -0,0 +1,7 @@
+package com.github.kmacdonald222.asclepiasfw;
+
+public class AsclepiasFW {
+	public void test(String input) {
+		System.out.println("Test called with input \"" + input + "\"");
+	}
+}
---
+++ AsclepiasFWTest/Source/com/github/kmacdonald222/asclepiasfwtest/AsclepiasFWTest.java
@@ -0,0 +1,10 @@
+package com.github.kmacdonald222.asclepiasfwtest;
+
+import com.github.kmacdonald222.asclepiasfw.AsclepiasFW;
+
+public class AsclepiasFWTest {
+	public static void main(String[] args) {
+		AsclepiasFW fw = new AsclepiasFW();
+		fw.test("Hello World!");
+	}
+}
