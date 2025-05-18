4642d95fd8941fb62b0d733508c043eda977c0ce
KMacDonald222 <keeganm742@gmail.com>
2025-05-18 14:31:46 -0300

    Removed build environment testing files

--- AsclepiasFW/Source/com/github/kmacdonald222/asclepiasfw/AsclepiasFW.java
+++
@@ -1,7 +0,0 @@
-package com.github.kmacdonald222.asclepiasfw;
-
-public class AsclepiasFW {
-	public void test(String input) {
-		System.out.println("Test called with input \"" + input + "\"");
-	}
-}
